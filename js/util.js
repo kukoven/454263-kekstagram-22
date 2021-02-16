d9b627e5f46af1854a573f9cc57d43a5b320a6bc
kukoven <kukoven@gmail.com>
2021-02-16 20:15:11 +0300

    Разбил проект на модули

---
+++ js/util.js
@@ -0,0 +1,22 @@
+// Функция возвращающая целое число [min, max]
+
+const getInteger = (min, max) => {
+  if (min < 0 || min > max) {
+    min = 0;
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+getInteger(0, 10);
+
+
+// Функция для проверки максимальной длины строки
+
+const getMaxStringWidth = (string, maxWidth) => {
+  return string.length <= maxWidth;
+};
+
+getMaxStringWidth('Функция для проверки максимальной длины строки', 140);
+
+export {getInteger};
